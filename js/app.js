9894cd7362f76f1f6a63dadb56634b9607d538e0
Peter Byriel <peter.byriel@gmail.com>
2014-03-18 19:04:25 +0100

    Added js
    Missing working connection through §.ajax()

--- js/app.js
+++ js/app.js
@@ -46,27 +46,30 @@ function hideResults() {
 }
 
 // Function to get title + set src of image
-var displayVideo = function (video) {
+var displayVideo = function (thisvideo) {
     // first append a div and addClass("videoitem");
     $("#grid").append("div").addClass("videoitem");
     // make it easy to refer to the last div
     var thisdiv = $("#grid").find("div").last();
     // then insert an h2 and set h2 equal to var title
-    var title = video.title;
+    var title = thisvideo.title;
     thisdiv.append("<h2>" + title + "</h2>");
     // then insert image el and set src equal to var image
-    var image = video.thumbnails.default.url; //link to image from api
+    var image = thisvideo.thumbnails.default.url; //link to image from api
     //thisdiv.append(WHAT).attr("src", image);
 };
 
 // Function to  display + start a video that is clicked
-// function playVideo();
-/* Dummy code below
-http://www.youtube.com/embed/VIDEO_ID
-<iframe id="ytplayer" type="text/html" width="640" height="390"
-  src="http://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
-  frameborder="0"/>
-  */
+var playVideo = function (thisvideo) {
+    var video_id= thisvideo.id;
+    var video_title= thisvideo.title;
+    var video_viewCount = thisvideo.viewCount;
+    // iframe gets embedded in a variable
+    var video_frame="<iframe width='640' height='385' src='http://www.youtube.com/embed/" +  video_id + "' frameborder='0' type='text/html'></iframe>";  
+    // the iframe gets embedded in the page
+    var videocontent="<div id='title'>" + video_title + "</div><div>" + video_frame + "</div><div id='count'>" + video_viewCount + " views</div>";
+    $("#container_vid").html(videocontent);
+};
 
 /*--- Main search functions go here ---*/
 
@@ -80,15 +83,15 @@ function newSearch(userInput) {
 // WHAT DATA DO I NEED TO PASS IN HERE?
 var link = "https://gdata.youtube.com/feeds/api/users/";
 var channelID = "UCsooa4yRKGN_zEE8iknghZA";
-var postfix = "?v=2.1";	// not sure what it should be
+// will need more data above, not sure what it should be
+var tryurl: link + channelID + uinput;
 
 // Function containing ajax-call
 var getVidsResult = function (userInput) {
     console.log("getVidsResult started");
     //The ajax-function
     var result = $.ajax({
-        url: link + channelID + userInput + postfix,
-        data: {site: 'youtube'},
+        url: tryurl,
         part: 'snippet',
         dataType: "jsonp",
         type: "GET"
@@ -116,7 +119,7 @@ $(document).ready(function () {
 //when page is ready, focus to input
     $('#input').focus();
     // get value of user input
-    uinput = $("#input").val();
+    uinput = $(this).val();
     // What to do on submit by enter
 	$('#input').keydown(function (event) {
         uinput = $("#input").val();
